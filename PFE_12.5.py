f8d0ee88de8212e080b5df091e43448033585342
Max O'Connor <max.oconnor@gmail.com>
2022-05-24 21:53:37 +0100

    Fixed small transposition error

--- PFE_12.5.py
+++ PFE_12.5.py
@@ -3,26 +3,26 @@ import re
 url = input("Please provide a URL:")
 text = ""
 try:
-spliturl = url.split("/")
-hostname = spliturl[2]
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect((hostname, 80))
-cmd = ('GET ' + url + ' HTTP/1.0\r\n\r\n').encode()
-mysock.send(cmd)
+    spliturl = url.split("/")
+    hostname = spliturl[2]
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock.connect((hostname, 80))
+    cmd = ('GET ' + url + ' HTTP/1.0\r\n\r\n').encode()
+    mysock.send(cmd)
 
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    text = text + data.decode()
-#print(text)
-paras = text.split("\r\n\r\n")
-#print(len(paras))
-body = paras[1]
-header = paras[0]
-print(body)
-print(header)
-mysock.close()
+    while True:
+        data = mysock.recv(512)
+        if len(data) < 1:
+            break
+        text = text + data.decode()
+    #print(text)
+    paras = text.split("\r\n\r\n")
+    #print(len(paras))
+    body = paras[1]
+    header = paras[0]
+    print(body)
+    print(header)
+    mysock.close()
 
 except:
 
